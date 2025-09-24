eb353d9eb6cb0d6f9c0514a889bfca8c4e6966c7
hi9876598765 <ito2555@gmail.com>
2025-09-24 11:51:21 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,16 @@
+package ex11_03;
+
+	public abstract class TangibleAsset extends Asset {
+	
+		String color;
+		
+		public TangibleAsset(String name,int price,String color) {
+		
+			super(name,price);
+			
+			this.color = color;
+		}
+	
+	
+		public String getColor() {return this.color;}
+	}
